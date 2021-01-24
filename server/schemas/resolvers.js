742c77801621a42919fae4ceaccf1d0ca31025cb
andytang02 <andytang02@gmail.com>
2021-01-24 14:11:46 -0800

    signup/login routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,47 @@ const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = User.findOne({_id: context.user._id})
+                    .select('-__v -password')
+                    .populate({
+                        path: 'accounts',
+                        populate: {
+                            path: 'transactions'
+                        }
+                    });
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+  
+            return {token, user};
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({email: email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+  
+            const token = signToken(user);
+            return {token, user};
+        },
+    }
 };
   
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,27 +2,39 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-
+    _id: ID
+    firstname: String
+    lastname: String
+    username: String
+    email: String
+    accounts: [Account]
   }
 
   type Account {
-
+    _id: ID
+    name: String
+    balance: Float
+    user: User
+    transactions: [Transaction]
   }
 
   type Transaction {
-
+    _id: ID
+    name: String
+    oldbalance: Float
+    newbalance: Float
+    account: Account
   }
 
   type Query {
     me: User
-    users: [User]
-    user(username: String!): User
   }
   
   type Mutation {
-
+    login(email: String!, password: String!): Auth
+    addUser(firstname: String!, lastname: String!, username: String!, email: String!, password: String!): Auth
   }
-  
+
   type Auth {
     token: ID!
     user: User
