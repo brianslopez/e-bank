a76186a4fb3fde67eae3e9e296e9c15a9758a02a
andytang02 <andytang02@gmail.com>
2021-01-25 01:19:09 -0800

    import path

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const { authMiddleware } = require('./utils/auth');
-
+const path = require('path');
 const {
   ApolloServer
 } = require('apollo-server-express');
