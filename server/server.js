1c96e8297c551d12f48d79fd711db37c841c9594
andytang02 <andytang02@gmail.com>
2021-01-25 01:36:53 -0800

    try deploy again

--- server/server.js
+++ server/server.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const {
-  ApolloServer
-} = require('apollo-server-express');
-
-const {
-  typeDefs,
-  resolvers
-} = require('./schemas');
 const db = require('./config/connection');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
@@ -20,15 +14,12 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-server.applyMiddleware({
-  app
-});
+server.applyMiddleware({ app });
 
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -40,6 +31,7 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
