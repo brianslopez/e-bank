a48f01651dcf923b7e00173790ca61444b064566
andytang02 <andytang02@gmail.com>
2021-01-25 01:04:45 -0800

    fix connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-require('dotenv').config()
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/ebank', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
